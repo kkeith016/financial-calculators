f977a47fba0e89ba1f8f6100469582a8d1dca217
Kylee <kkeith.biz@gmail.com>
2025-09-26 12:51:07 -0400

    Future Value First Commit

--- src/main/java/com/pluralsight/futureValue.java
+++ src/main/java/com/pluralsight/futureValue.java
@@ -2,6 +2,6 @@ package com.pluralsight;
 
 public class futureValue {
     public static void main(String[] args) {
-
+double futureValue = principle * Math.pow(1+(annualInterest/365)((365 * totalNumberofDays)))
     }
 }
