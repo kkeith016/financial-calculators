042b6260fa4a86cd3533256ad8bdbf223d361b58
Kylee <kkeith.biz@gmail.com>
2025-09-26 14:49:56 -0400

    PV user inputs

--- src/main/java/com/pluralsight/presentValue.java
+++ src/main/java/com/pluralsight/presentValue.java
@@ -1,7 +1,27 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class presentValue {
     public static void main(String[] args) { ;
+Scanner scan = new Scanner(System.in);
+
+//user inputs
+
+        System.out.print("Enter the monthly payout");
+        int monthlyPayout = scan.nextInt();
+
+        System.out.print("Enter the annual interest rate");
+        int rate = scan.nextInt();
+
+        System.out.print("Enter the number of years");
+        int year = scan.nextInt();
+
+        double monthlyRate = rate / 100 / 12;
+        int totalPayments = year * 12;
+
+        // Math
+
 
     }
 }
