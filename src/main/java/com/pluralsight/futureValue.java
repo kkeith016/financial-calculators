5f6cdc05edb73ddd0415c8d6b6b3562ecb671a00
Kylee <kkeith.biz@gmail.com>
2025-09-26 13:14:08 -0400

    total breakdown

--- src/main/java/com/pluralsight/futureValue.java
+++ src/main/java/com/pluralsight/futureValue.java
@@ -7,7 +7,6 @@ public class futureValue {
         Scanner scan =  new Scanner(System.in);
 
         //getting the inputs
-
         System.out.print("Enter Deposit Amount: ");
         double depositAmount = scan.nextDouble();
 
@@ -17,16 +16,19 @@ public class futureValue {
         System.out.print("Total years: ");
         int years = scan.nextInt();
 
-// calculating interest to a decimal
+        // calculating interest to a decimal
         double rate = interest / 100.0;
         double dailyInterest = rate / 365;
 
         //total number of days
         int days = years * 365;
 
-
-
-
-
+        // calculate and print
+        double futureValue = depositAmount * Math.pow(1 + dailyInterest, days);
+        double totalinterest = futureValue - depositAmount;
+        System.out.println("----Total Breakdown----");
+        System.out.println("After a total of " + years + " years");
+        System.out.printf("Total Future Value: $%.2f%n", futureValue);
+        System.out.printf("Total Interest Value: $%.2f%n", totalinterest);
     }
 }
